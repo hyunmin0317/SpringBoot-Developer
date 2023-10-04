d7e4a2b5655a3659a2242684c0c912f7fb35daf0
hyunmin0317 <choihm9903@naver.com>
2023-10-04 20:24:35 +0900

    feat: Article list API 작성 (#7)

--- src/main/java/com/hyunmin/springboot/controller/BlogApiController.java
+++ src/main/java/com/hyunmin/springboot/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.hyunmin.springboot.controller;
 
 import com.hyunmin.springboot.domain.Article;
 import com.hyunmin.springboot.dto.AddArticleRequest;
+import com.hyunmin.springboot.dto.ArticleResponse;
 import com.hyunmin.springboot.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -20,5 +24,14 @@ public class BlogApiController {
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok().body(articles);
     }
 }
--- src/main/java/com/hyunmin/springboot/service/BlogService.java
+++ src/main/java/com/hyunmin/springboot/service/BlogService.java
@@ -6,6 +6,8 @@ import com.hyunmin.springboot.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,9 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
