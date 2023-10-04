2a718358bcf5d9fff8a59f90add665976978313d
hyunmin0317 <choihm9903@naver.com>
2023-10-04 22:12:37 +0900

    feat: Article read 뷰 작성 (#8)

--- src/main/java/com/hyunmin/springboot/controller/BlogViewController.java
+++ src/main/java/com/hyunmin/springboot/controller/BlogViewController.java
@@ -1,14 +1,14 @@
 package com.hyunmin.springboot.controller;
 
+import com.hyunmin.springboot.domain.Article;
 import com.hyunmin.springboot.dto.ArticleListViewResponse;
+import com.hyunmin.springboot.dto.ArticleViewResponse;
 import com.hyunmin.springboot.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.List;
 
 
@@ -26,5 +26,12 @@ public class BlogViewController {
                 .toList();
         model.addAttribute("articles", articles);
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+        return "article";
     }
 }
