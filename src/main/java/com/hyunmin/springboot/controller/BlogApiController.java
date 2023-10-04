0ea309f58b6a227d2c53616e969314c7cf6c844f
hyunmin0317 <choihm9903@naver.com>
2023-10-04 20:16:05 +0900

    feat: Article create API 작성 (#7)

---
+++ src/main/java/com/hyunmin/springboot/controller/BlogApiController.java
@@ -0,0 +1,24 @@
+package com.hyunmin.springboot.controller;
+
+import com.hyunmin.springboot.domain.Article;
+import com.hyunmin.springboot.dto.AddArticleRequest;
+import com.hyunmin.springboot.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+}
