037e03764908d8562a1779a12b0bb1f19800167d
hyunmin0317 <choihm9903@naver.com>
2023-10-05 20:57:06 +0900

    feat: RefreshToken 리포지토리 작성 (#10)

---
+++ src/main/java/com/hyunmin/springboot/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.hyunmin.springboot.repository;
+
+import com.hyunmin.springboot.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
