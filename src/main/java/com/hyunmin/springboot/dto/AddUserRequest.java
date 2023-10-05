5b8da6a7c4129b26c5634a2856e1b025867b570e
hyunmin0317 <choihm9903@naver.com>
2023-10-05 20:12:00 +0900

    feat: AddUserRequest dto 작성 (#8)

---
+++ src/main/java/com/hyunmin/springboot/dto/AddUserRequest.java
@@ -0,0 +1,12 @@
+package com.hyunmin.springboot.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
